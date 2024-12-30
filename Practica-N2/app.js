a7c4a1f0c875055a233bc9c25ee4b87041b99a8e
Franco <aesevv@gmail.com>
2024-12-30 12:31:41 -0300

    marquee infinito

--- Practica-N2/app.js
+++ Practica-N2/app.js
@@ -2,3 +2,4 @@ let paragraph = document.querySelector(".text")
 let text = "Franco".repeat(400)
 paragraph.textContent = text
 paragraph.innerHTML = paragraph.textContent.replace(/\S/g,"<span>$&</span>")
+// asd
